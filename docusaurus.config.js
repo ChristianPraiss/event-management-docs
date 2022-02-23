867dc2251e137dd9159319d7f9321091e3359b3e
Christian Praiß <christian_praiss@icloud.com>
2022-02-23 09:40:21 +0100

    feat(docs): initialized docs with localization and versioning

---
+++ docusaurus.config.js
@@ -0,0 +1,72 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Docs',
+  tagline: 'Documentation for dispo tools',
+  url: 'https://docs.dispo.tools',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    localeConfigs: {
+      en: {
+        htmlLang: 'en-US',
+      }
+    },
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        blog: {
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Dispo Tools',
+        logo: {
+          alt: 'Dispo Tools Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'}
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+import Translate from '@docusaurus/Translate';
+
+function HomepageHeader() {
+    return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+          <h1 className="hero__title"><Translate>homepage.welcome</Translate></h1>
+        <p className="hero__subtitle"><Translate>homepage.welcome_description</Translate></p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description={`${siteConfig.tagline}`}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
